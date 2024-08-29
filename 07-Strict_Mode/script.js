8be9f06c8eed425eeebb2fc79b31e97fec54993d
Surya-2k4 <suryanatarajan04@gmail.com>
2024-08-29 21:45:46 +0530

    Explored & implement strict- mode  concept in javascript

---
+++ 07-Strict_Mode/script.js
@@ -0,0 +1,16 @@
+//'use strict';  - uncomment for enable strict mode
+// When strict mode is enabled, some silent errors are converted to throw errors, which makes debugging easier
+
+console.log('Strict Mode');
+
+let user = 'surya';
+
+use = 'siva';
+
+console.log(user);
+
+const interface = 'CAr';  
+console.log(interface);
+//interface , private are reserved keywords, we cant use that for variable name
+const private = 'Jack';
+console.log(private); 
